3913d36aa7653616572a6967a6983151cbe947e7
Phil Murphy <phillipdeanmurphy@gmail.com>
2021-08-05 23:31:21 -0700

    pushing this one more time for another submission attempt :(

--- server.js
+++ server.js
@@ -22,8 +22,7 @@ class Forecast {
 
 class Movie {
   constructor(movie){
-    this.title = movie.data.results.original_title;
-    this.src = `http://image.tmdb.org/t/p/w500${movie.data.results.poster_path}`;
+    this.src = `https://image.tmdb.org/t/p/w500${movie.data.results.poster_path}`;
     this.alt = movie.data.results.title;
   }
 }
@@ -35,8 +34,7 @@ app.get('/', (req, res) => {
 app.get('/weather', async (request, response) => {
   let lat = request.query.lat;
   let lon = request.query.lon;
-  console.log(lat, lon);
-  let wxArr = [];
+
   let wxData = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`);
   try {
     response.send(wxData.data.data.map(day =>
@@ -49,12 +47,11 @@ app.get('/weather', async (request, response) => {
 
 app.get('/movies', async (request, response) => {
   console.log('I am here');
-  let city = request.query.city;
+  let query = request.query.query;
 
-  let movieData = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${city}`);
-  console.log(movieData.data.results);
+  let movieData = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${query}`);
   try {
-    response.send(movieData.data.results.map(movie =>
+      response.send(movieData.data.results.map(movie =>
       new Movie(movie)));
   } catch {
     response.status(404).send('Whoops, seems like there\'s a problem');
